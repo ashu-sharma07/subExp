e3c0bdd4dfcb78dfc769665ce22065f93f13788f
Ashu Sharma <ashusharma0771@gmail.com>
2023-03-01 11:15:33 +0530

    add 404

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.get("/app", (req, res) => {
   res.send("App is working");
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    message:
+      "Ohh you are lost, read the API documentation to find your way back home :)",
+  });
+});
+
 app.listen(3000, () => {
   console.log("server running on port 3000");
 });
